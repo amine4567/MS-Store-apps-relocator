f118fe1c47df1457a00c3e896b9dfdbadba80b98
amine4567 <souikiamine@gmail.com>
2021-05-19 04:21:35 +0200

    [UI/Back] feat/wip: select the app to dump in a separate dialog and show more information (pid, icon, executable location)

--- src/ui.py
+++ src/ui.py
@@ -1,7 +1,7 @@
 import re
 import sys
 
-from PySide6 import QtCore, QtWidgets
+from PySide6 import QtCore, QtWidgets, QtGui
 
 from uwp import get_running_uwp_apps
 
@@ -28,24 +28,25 @@ class MyWidget(QtWidgets.QWidget):
 
         # Widgets definitions
         self.uwp_process_label = QtWidgets.QLabel("UWP process")
+        self.uwp_process = QtWidgets.QLineEdit()
+        self.select_uwp_btn = QtWidgets.QPushButton("Select")
+        self.uwp_selecting_dialog = QtWidgets.QDialog(
+            f=QtCore.Qt.WindowMaximizeButtonHint | QtCore.Qt.WindowCloseButtonHint
+        )
         self.uwp_processes_dropdown = QtWidgets.QComboBox()
         self.refresh_btn = QtWidgets.QPushButton("Refresh list")
         self.destn_dir_label = QtWidgets.QLabel("Destination directory")
         self.destn_dir = QtWidgets.QLineEdit()
-        self.select_destn_btn = QtWidgets.QPushButton("Select")
+        self.select_destn_btn = QtWidgets.QPushButton("Browse")
         self.dump_btn = QtWidgets.QPushButton("Dump")
         self.dumping_dialog = QtWidgets.QDialog(
             f=QtCore.Qt.WindowMaximizeButtonHint | QtCore.Qt.WindowCloseButtonHint
         )
 
         # Set widgets properties/data
-        self.running_uwp_apps = get_running_uwp_apps()
-        self.uwp_processes_dropdown.insertItems(
-            0, [elt["image_name"] for elt in self.running_uwp_apps]
-        )
-
         self.destn_dir.setReadOnly(True)
 
+        self.select_uwp_btn.clicked.connect(self.select_uwp_process)
         self.refresh_btn.clicked.connect(self.refresh_list)
         self.select_destn_btn.clicked.connect(self.select_destn_folder)
         self.dump_btn.clicked.connect(self.dump_app)
@@ -56,10 +57,8 @@ class MyWidget(QtWidgets.QWidget):
         self.layout.addWidget(
             self.uwp_process_label, 0, 0, 1, 2, alignment=QtCore.Qt.AlignTop
         )
-        self.layout.addWidget(
-            self.uwp_processes_dropdown, 1, 0, alignment=QtCore.Qt.AlignTop
-        )
-        self.layout.addWidget(self.refresh_btn, 1, 1, alignment=QtCore.Qt.AlignTop)
+        self.layout.addWidget(self.uwp_process, 1, 0, alignment=QtCore.Qt.AlignTop)
+        self.layout.addWidget(self.select_uwp_btn, 1, 1, alignment=QtCore.Qt.AlignTop)
         self.layout.addWidget(
             self.destn_dir_label, 2, 0, 1, 2, alignment=QtCore.Qt.AlignTop
         )
@@ -68,6 +67,33 @@ class MyWidget(QtWidgets.QWidget):
 
         self.layout.addWidget(self.dump_btn, 4, 0, 1, 2, alignment=QtCore.Qt.AlignTop)
         self.layout.setRowStretch(3, 1)
+
+        # Selecting UWP process dialog
+        self.running_uwp_apps_table = QtWidgets.QTableWidget()
+        self.running_uwp_apps_table.setEditTriggers(
+            QtWidgets.QAbstractItemView.NoEditTriggers
+        )
+        self.running_uwp_apps_table.setSelectionMode(
+            QtWidgets.QAbstractItemView.SingleSelection
+        )
+        self.running_uwp_apps_table.setSelectionBehavior(
+            QtWidgets.QAbstractItemView.SelectRows
+        )
+        cols_header = ["PID", "Icon", "Process", "Executable path"]
+        self.running_uwp_apps_table.setColumnCount(len(cols_header))
+        self.running_uwp_apps_table.setHorizontalHeaderLabels(cols_header)
+
+        self.update_running_uwp_table()
+
+        self.running_uwp_apps_table.resizeRowsToContents()
+        self.running_uwp_apps_table.resizeColumnsToContents()
+
+        self.uwp_selecting_dialog.setModal(True)
+
+        self.uwp_selecting_layout = QtWidgets.QGridLayout(self.uwp_selecting_dialog)
+
+        self.uwp_selecting_layout.addWidget(self.running_uwp_apps_table)
+        self.uwp_selecting_layout.addWidget(self.refresh_btn)
 
         # Dumping dialog
         self.uwpdumper_output = QtWidgets.QTextBrowser()
@@ -79,13 +105,39 @@ class MyWidget(QtWidgets.QWidget):
         self.dumping_dialog_layout.addWidget(QtWidgets.QLabel("UWPDumper output:"))
         self.dumping_dialog_layout.addWidget(self.uwpdumper_output)
 
+    def update_running_uwp_table(self):
+        self.running_uwp_apps = get_running_uwp_apps()
+
+        self.running_uwp_apps_table.setRowCount(len(self.running_uwp_apps))
+        for i, running_app_data in enumerate(self.running_uwp_apps):
+            self.running_uwp_apps_table.setItem(
+                i,
+                0,
+                QtWidgets.QTableWidgetItem(
+                    running_app_data["pid"],
+                ),
+            )
+
+            pixmap = QtGui.QPixmap(running_app_data["logo_fullpath"])
+            icon_item = QtWidgets.QTableWidgetItem()
+            icon_item.setIcon(QtGui.QIcon(pixmap))
+            self.running_uwp_apps_table.setItem(i, 1, icon_item)
+
+            self.running_uwp_apps_table.setItem(
+                i, 2, QtWidgets.QTableWidgetItem(running_app_data["image_name"])
+            )
+            self.running_uwp_apps_table.setItem(
+                i, 3, QtWidgets.QTableWidgetItem(running_app_data["executable_path"])
+            )
+
     @QtCore.Slot()
     def refresh_list(self):
         self.uwp_processes_dropdown.clear()
-        self.running_uwp_apps = get_running_uwp_apps()
-        self.uwp_processes_dropdown.insertItems(
-            0, [elt["image_name"] for elt in self.running_uwp_apps]
-        )
+        self.update_running_uwp_table()
+
+    @QtCore.Slot()
+    def select_uwp_process(self):
+        self.uwp_selecting_dialog.show()
 
     @QtCore.Slot()
     def select_destn_folder(self):
--- src/uwp.py
+++ src/uwp.py
@@ -1,8 +1,51 @@
 from typing import List, Dict
 import subprocess
+import os
+import time
+import re
+from functools import lru_cache
+
+import psutil
+
+
+@lru_cache(maxsize=32)
+def get_installed_uwp_apps():
+    all_installed_uwps = subprocess.run(
+        "powershell Get-AppxPackage",
+        capture_output=True,
+    ).stdout.decode()
+    all_installed_uwps = re.sub("\\r\\n +", " ", all_installed_uwps)
+
+    all_uwps_data = [
+        dict(
+            [
+                list(map(lambda x: x.strip(), elt.split(":", maxsplit=1)))
+                for elt in uwp_data.split("\r\n")
+            ]
+        )
+        for uwp_data in all_installed_uwps.strip().split("\r\n\r\n")
+    ]
+
+    all_uwps_data_dict = {elt["PackageFullName"]: elt for elt in all_uwps_data}
+    return all_uwps_data_dict
+
+
+class UWPAppManifest:
+    def __init__(self, pkg_name: str):
+        self._manifest_str = subprocess.run(
+            f"powershell (Get-AppxPackageManifest -Package {pkg_name}).OuterXml",
+            capture_output=True,
+        ).stdout.decode()
+
+    def get_name(self) -> str:
+        return re.search(r'<Identity Name="(.*?)"', self._manifest_str).group(1)
+
+    def get_logo_location(self) -> str:
+        return re.search(r"<Logo>(.*?)</Logo>", self._manifest_str).group(1)
 
 
 def get_running_uwp_apps() -> List[Dict[str, str]]:
+    t1 = time.time()
     cmd_output = subprocess.run(
         'tasklist /apps /fo list /fi "status eq running"', capture_output=True
     )
@@ -17,4 +60,38 @@ def get_running_uwp_apps() -> List[Dict[str, str]]:
         }
         for row in cmd_output_list
     ]
+
+    all_installed_uwp_apps = get_installed_uwp_apps()
+
+    # Get additional data about the running uwp apps
+    for uwp_data in cmd_output_list:
+        pid = int(uwp_data["pid"])
+        proc = psutil.Process(pid)
+
+        installed_uwp_data = all_installed_uwp_apps[uwp_data["pkg_name"]]
+        app_name = installed_uwp_data["Name"]
+
+        install_location = installed_uwp_data["InstallLocation"]
+
+        with open(os.path.join(install_location, "AppxManifest.xml"), "r") as f:
+            manifest = f.read()
+            logo_relative_path = re.search(r"<Logo>(.*?)</Logo>", manifest).group(1)
+
+        uwp_data["executable_path"] = proc.exe()
+        uwp_data["app_name"] = app_name
+        uwp_data["install_location"] = install_location
+        uwp_data["logo_relative_path"] = logo_relative_path
+
+        logos_dir, logo_pattern_name = logo_relative_path.rsplit("\\", maxsplit=1)
+        logo_filename = [
+            filename
+            for filename in os.listdir(os.path.join(install_location, logos_dir))
+            if filename.startswith(logo_pattern_name.split(".")[0])
+        ][0]
+        uwp_data["logo_fullpath"] = os.path.join(
+            install_location, logos_dir, logo_filename
+        )
+
+    t2 = time.time()
+    print(f"get_running_uwp_apps took {t2-t1} seconds")
     return cmd_output_list
